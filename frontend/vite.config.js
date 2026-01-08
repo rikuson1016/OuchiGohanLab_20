efe7cf0bfcd0a8e167418b77bc5748a6a9e605c7
rikuson1016 <riku.tamatai2929@gmail.com>
2026-01-09 00:06:31 +0900

    GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/OuchiGohanLab_20/', // リポジトリ名をここに入れる
   plugins: [
     vue(),
     vueDevTools(),
